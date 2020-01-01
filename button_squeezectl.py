cd5be68452372ac6cf206e0840dadf9a0634f42c
baztian <bastian.dev@gmail.com>
2020-01-01 21:00:43 +0100

    Button control

--- button_squeezectl.py
+++ button_squeezectl.py
@@ -1,33 +1,51 @@
 #!/usr/bin/env python3
 
-from signal import pause
+import signal
 
 from gpiozero import Button
 from lms import Server
 
-def loop(host, port, player_id):
-  Button(15).wait_for_press()
+class ButtonHandler:
+  def __init__(self, host, port, player_id):
+    self._host = host
+    self._port = port
+    self._player_id = player_id
 
-  server = Server(host, port)
-  print(server)
-  p=server.player(player_id)
-  p.play()
-  p.pause()
-  p.seek('-10')
+  def play_pause(self, button):
+    if button.is_held:
+      return
+    print("play/pause")
+    self._player.query('pause')
 
-  p.query('playlistcontrol', 'cmd:load', 'playlist_name:44-21-65-188-196')
-  p.query('playlist', 'name', '?')
-  p.query('playlist', 'playlistsinfo')
+  def seek_forward(self):
+    print("seeking+20")
+    self._player.seek('+20')
+
+  def loop(self):
+    server = Server(self._host, self._port)
+    server.update()
+    print(server)
+    self._player=server.player(self._player_id)
+
+    button = Button(15, hold_repeat=True, hold_time=3)
+    button.when_released = self.play_pause
+    button.when_held = self.seek_forward
+  #  p.seek('-10')
+
+  #  p.query('playlistcontrol', 'cmd:load', 'playlist_name:44-21-65-188-196')
+  #  p.query('playlist', 'name', '?')
+  #  p.query('playlist', 'playlistsinfo')
+    signal.pause()
 
 def main():
   import sys
-  print(len(sys.argv))
   if len(sys.argv) != 4:
     print("Usage: button-squeezectl lms_host port player_id", file=sys.stderr)
     print("Example: button-squeezectl music 9000 b8:27:eb:d0:cc:13", file=sys.stderr)
     sys.exit(1)
   else:
-    loop(sys.argv[1], int(sys.argv[2]), sys.argv[3])
+    handler = ButtonHandler(sys.argv[1], int(sys.argv[2]), sys.argv[3])
+    handler.loop()
 
 if __name__ == "__main__":
   main()
